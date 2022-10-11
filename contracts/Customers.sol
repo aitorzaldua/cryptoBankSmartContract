75829cf27ddb58a485bfb57e173705ba837bc101
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-10-11 19:04:41 +0200

    Setting roles into Customers.sol

--- contracts/Customers.sol
+++ contracts/Customers.sol
@@ -1,24 +1,70 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
-import "./Roles.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Customers is Roles {
+contract Customers is AccessControl, Ownable {
 
-struct Client {
-    uint clientId;
-    string name;
-    string surname;
-    string physicalAddress;
-    string passport;
-    address clientAccount;
-}
+    uint public customerId;
 
-function requestToBeClient() public {
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant USER_ROLE = keccak256("USER_ROLE");
+    bytes32 public constant REQUESTER_ROLE = keccak256("REQUESTER_ROLE");
 
-    _setupRole(ROLE_REQUESTER, msg.sender);
+    constructor() {
+        customerId = 0;
 
-}
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(ADMIN_ROLE, msg.sender);
+        _setupRole(USER_ROLE, msg.sender);
+        _setupRole(REQUESTER_ROLE, msg.sender);
+
+    }
+
+    struct singleCustomer {
+        string name;
+        string surname;
+        string physicalAddress;
+        string passport;
+        address clientAccount;
+        bytes32 role;
+    }
+
+    mapping(uint => singleCustomer) CustomersDataBase;
+
+
+    function requestToBeCustomer (string memory _name, string memory _surname, string memory _physicalAddress, string memory _passport) public {
+
+        _setupRole(REQUESTER_ROLE, msg.sender);
+
+        singleCustomer storage newSingleCustomer = CustomersDataBase[customerId];
+        newSingleCustomer.name = _name;
+        newSingleCustomer.surname = _surname;
+        newSingleCustomer.physicalAddress = _physicalAddress;
+        newSingleCustomer.passport = _passport;
+        newSingleCustomer.clientAccount = msg.sender;
+        newSingleCustomer.role = keccak256("REQUESTER_ROLE");
+
+        customerId++;
+
+    }
+
+    function getCustomers (uint _customerId) public view returns (string memory, string memory, string memory, string memory, address) {
+
+        singleCustomer storage data = CustomersDataBase[_customerId];
+        return (data.name, data.surname, data.physicalAddress, data.passport, data.clientAccount);
+
+    }
+
+     //Change Roles:
+    function addRole(bytes32 role, address account) public onlyRole(ADMIN_ROLE){
+        _grantRole(bytes32(role), account);
+    }
+
+    function removeRole(bytes32 role, address account) public onlyRole(ADMIN_ROLE){
+        _revokeRole(role, account);
+    }
+
 }
