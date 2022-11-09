5f77a81aa8d701f1a073be23199ee9d5c18b5571
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-11-09 18:17:54 +0100

    add env . deploy and verify

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,33 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('@openzeppelin/hardhat-upgrades');
+require ("@nomiclabs/hardhat-etherscan");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.4",
+  networks: {
+    mumbai: {
+      url: process.env.POLYGON_MUMBAY_NODE,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGON_MUMBAI_API_KEY
+  }
 };
+
